3059d2db0eae923145a7cf6768b388551bddf55c
Gasymov Rustam <rustam.gasymov@devalore.com>
2022-09-14 10:56:03 +0300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import { Suspense } from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 
-import './index.scss';
-import { AboutPageAsync, MainPageAsync } from './pages';
+import { AboutPageAsync, MainPageAsync } from "./pages";
+import "./styles/index.scss";
+import { useTheme } from "./theme";
 
 const App = () => {
-    return (
-        <div className="app">
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
-                </Routes>
-            </Suspense>
-            
-        </div>
-    );
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
