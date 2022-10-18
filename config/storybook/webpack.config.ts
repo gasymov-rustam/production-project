c37921ff81eb437d8537a8e181a0239fc221167e
Rustam Gasymov <rustam.gasymov@devalore.com>
2022-10-18 05:17:51 +0300

    fix test and storybook config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -5,12 +5,11 @@ import type { BuildPaths } from '../build';
 
 export default ({ config }: { config: Configuration }) => {
   const paths: Pick<BuildPaths, 'src'> = { src: path.resolve(__dirname, '..', '..', 'src') };
-  config.resolve?.modules?.push(paths.src);
-  config.resolve?.extensions?.push('.ts', '.tsx');
+  config.resolve!.modules!.push(paths.src);
+  config.resolve!.extensions!.push('.ts', '.tsx');
   config.module?.rules?.push(buildCssLoader(true));
   // @ts-ignore
-  // eslint-disable-next-line no-param-reassign
-  config.module?.rules = config?.module?.rules?.map((rule: RuleSetRule) => {
+  config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
     }
@@ -18,12 +17,12 @@ export default ({ config }: { config: Configuration }) => {
     return rule;
   });
 
-  config.module?.rules?.push({
+  config.module!.rules!.push({
     test: /\.svg$/,
     use: ['@svgr/webpack'],
   });
 
-  config.plugins?.push(
+  config.plugins!.push(
     new DefinePlugin({
       __IS_DEV__: true,
       __API__: JSON.stringify(''),
