ef547e0b729c01e2fbcdf83b9d3696a9ccf9afad
Rustam Gasymov <rustam.gasymov@devalore.com>
2022-11-16 08:46:51 +0200

    fix axios instance, add config netlify for single page application, add to build production script url for production

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,12 @@ import { USER_LOCAL_STORAGE_KEY } from '../constants';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) ?? '',
-  },
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+  }
+
+  return config;
 });
