e1e133ef13c7c54ae4198c79b0a3d4a12ec52192
Rustam Gasymov <rustam.gasymov@devalore.com>
2022-11-16 08:41:44 +0200

    remove all setTimeouts on pages with lazy loading

--- src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
+++ src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
@@ -2,11 +2,4 @@ import { FC, lazy } from 'react';
 
 import { AddCommentFormProps } from './AddCommentForm';
 
-export const AddCommentFormAsync = lazy<FC<AddCommentFormProps>>(
-  () =>
-    new Promise((resolve) => {
-      // @ts-ignore
-      // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-      setTimeout(() => resolve(import('./AddCommentForm')), 400);
-    }),
-);
+export const AddCommentFormAsync = lazy<FC<AddCommentFormProps>>(() => import('./AddCommentForm'));
